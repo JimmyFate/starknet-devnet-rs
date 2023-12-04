34b775e792d5cf6d600b9ea413eef51a4b8c4d0e
FabijanC <fabijan.corak@gmail.com>
2023-12-04 14:43:28 +0100

    echo if failed

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -2,10 +2,12 @@
 
 set -eu
 
-curl -L https://foundry.paradigm.xyz | bash
+echo "Installing foundryup"
+curl -L https://foundry.paradigm.xyz | bash || echo "As expected, received a non-zero exit code"
 export PATH="$PATH:/home/fabijanc/.foundry/bin"
 
 # TODO if necessary, add the above PATH modification to BASH_ENV on circleci
 
+echo "Installing foundry"
 foundryup
 anvil --version
